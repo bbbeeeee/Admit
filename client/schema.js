a0de39954977cef8527f8b5e869a8aaa5fd9d980
Brandon Truong <me@brandontruong.com>
2014-05-13 16:59:08 -0500

    Testing with autoform and the rough schema

--- client/schema.js
+++ client/schema.js
@@ -1,6 +1,149 @@
 if (Meteor.isClient) {
+  activitySchema = new SimpleSchema({
+    activityType: {
+      type: String
+    },
+    description: {
+      type: String,
+      max: 200
+    },
+    durationInYears: {
+      type: Number,
+      max:1
+    }
+  });
 
-  Apps = new Meteor.Collection("applications", {
+  testScoreSchema = new SimpleSchema({
+    testName: {
+      type: String
+    },
+    testDate: {
+      type: Date
+    },
+    testScore: {
+      type: Number,
+    }
+  });
+
+  testSchema = new SimpleSchema({
+    date: {
+    type: Date
+   },
+    tag: {
+    type: String
+    },
+   score: {
+    type: Number
+    }
+  });
+
+
+  gradesSchema = new SimpleSchema({
+    rank: {
+     type: Number
+    },
+    classSize: {
+     type: Number
+    },
+    gpaWeighted: {
+     type: Boolean
+    },
+    gpaScale: {
+     type: Number
+    },
+    GPA: {
+     type: Number
+    }
+  });
+
+  collegeSchema = new SimpleSchema({
+    collegeName: {
+      type: String,
+      max: 200
+    },
+    topChoice: {
+      type: Boolean
+    },
+    earlyApplication: {
+      type: Boolean
+    },
+    legacy: {
+      type: Boolean
+    },
+    athleticRecruit: {
+      type: Boolean
+    }
+
+  });
+
+  AddressSchema = new SimpleSchema({
+    street: {
+      type: String,
+      max: 100
+    },
+    city: {
+      type: String,
+      max: 50
+    },
+    state: {
+      type: String,
+      regEx: /^A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]$/
+    },
+    country: {
+      type: String,
+      max: 40
+    },
+    zip: {
+      type: String,
+      regEx: /^[0-9]{5}$/
+    }
+  });
+
+  highSchoolSchema = new SimpleSchema({
+    name: {
+      type: String,
+      max: 200
+    },
+    CEEBCode: {
+      type: Number,
+      max: 6
+    },
+    address: {
+      type: AddressSchema
+    },
+    phoneNumber: {
+      type: Number,
+      regEx: /^[0-9]{10}$/
+    }
+  });
+
+  longEssaySchema = new SimpleSchema({
+    essayType: {
+      type: String 
+    },
+    plainText: {
+      type: String,
+      regEx: /[[a-zA-Z0-9]+\s]{650}$/g // this may need modification.  It looks for one or more non-whitespace characters followed by zero or more whitespace chacaters and ensures newline characte ar ine included in whitespace count
+    }
+  });
+
+
+  honorSchema = new SimpleSchema({
+    description: {
+      type: String,
+      max: 200
+    },
+    durationInYears: {
+      type: Number,
+      max:1
+    },
+    levelOfRecognition: {
+      type: String,
+      max: 3
+    }
+  });
+
+  Apps = new Meteor.Collection("apps", {
     schema: {
      firstName: {
       type: String,
@@ -65,10 +208,10 @@ if (Meteor.isClient) {
       regEx: SimpleSchema.RegEx.Email
      },
      counselorContactApproval: {
-      type: boolean
+      type: Boolean
      },
      veteranStatus: {
-      type: boolean
+      type: Boolean
      },
      activities: {
       type: [activitySchema]
@@ -85,154 +228,7 @@ if (Meteor.isClient) {
      AP_IB_SATSUBJECTS: {
       type: [testSchema]
      }
-
-  }});
-
-activitySchema = new SimpleSchema({
-  activityType: {
-    type: String
-  },
-  description: {
-    type: String,
-    max: 200
-  },
-  durationInYears: {
-    type: Number,
-    max 1
-  }
-});
-
-// flesh out these standardized test score schemas 
-testScoreSchema = new SimpleSchema({
-  testName: {
-    type: String
-  },
-  testDate: {
-    type: Date
-  },
-  testScore: {
-    type: Number,
-  }
-});
-
-testSchema = new SimpleSchema({
-  dte: {
-  type: Date
- },
-  ag: {
-  type: String
-  ,
- score: {
-  type: Number
-  }
-});
-
-
-gradesSchema = new SimpleSchema({
-  rank: {
-   type: Number
-  },
-  classSize: {
-   type: Number
-  },
-  gpaWeighted: {
-   type: boolean
-  },
-  gpaScale: {
-   type: Number
-  },
-  GPA: {
-   type: Number
-  }
-});
-
-collegeSchema = new SimpleSchema({
-  collegeName: {
-    type: String,
-    max: 200
-  },
-  topChoice: {
-    type: boolean
-  },
-  earlyApplication: {
-    type: boolean
-  },
-  legacy: {
-    type: boolean
-  },
-  athleticRecruit: {
-    type: boolean
-  }
-
-});
-
-
-highSchoolSchema = new SimpleSchema({
-  name: {
-    type: String,
-    max: 200
-  },
-  CEEBCode: {
-    type: Number,
-    max: 6
-  },
-  address: {
-    type: AddressSchema
-  },
-  phoneNumber: {
-    type: Number,
-    regEx: /^[0-9]{10}$/
-  }
-});
-
-AddressSchema = new SimpleSchema({
-  street: {
-    type: String,
-    max: 100
-  },
-  city: {
-    type: String,
-    max: 50
-  },
-  state: {
-    type: String,
-    regEx: /^A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]$/
-  },
-  country: {
-    type: String,
-    max: 40
-  }
-  zip: {
-    type: String,
-    regEx: /^[0-9]{5}$/
-  }
-});
-
-longEssaySchema = new SimpleSchema({
-  essayType: {
-    type: String 
-  },
-  plainText: {
-    type: String
-    regEx: /\S+\s*{600}$/g // this may need modification.  It looks for one or more non-whitespace characters followed by zero or more whitespace chacaters and ensures newline characte ar ine included in whitespace count
-  }
-})
-
-
-honorSchema = new SimpleSchema({
-  description: {
-    type: String,
-    max: 200
-  },
-  durationInYears: {
-    type: Number,
-    max 1
-  },
-  levelOfRecognition: {
-    type: String,
-    max: 3
-  }
-});
-
+    }
+  });
 
 }
--- server.js
+++ server.js
@@ -2,14 +2,11 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-  console.log(Meteor.users.findOne());
  
   // Accounts.createUser({ 
   //  username: "user",
   //  email: "me@brandontruong.com",
   //  password: "password"
   // });
- if(Meteor.users.findOne() && Meteor.users.find().length === 0){
 
-  }
 }
